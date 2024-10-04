d0398d432e78f0b68bbeb0d81d8609a224f2bf1a
Alican <alicbozkurt@gmail.com>
2024-10-04 05:10:08 +0300

    update

---
+++ terraform_aws_v1/dev/main.tf
@@ -0,0 +1,68 @@
+# Providers
+provider "aws" {
+  region = "us-east-1"  
+}
+
+
+# Call the Networking Module
+module "networking" {
+  source                     = "../modules/networking"
+  env                        = var.env
+  vpc_cidr_block             = "10.0.0.0/16"  # Or pass from variables
+  public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
+  allowed_ssh_cidr_blocks    = ["0.0.0.0/0"]  # Replace with your IP range
+}
+
+module "terraform_data" {
+  source         = "../modules/terraform_data"
+  env            = var.env
+  objects        = var.objects
+  subenv_objects = var.subenv_objects
+}
+
+module "ecs_service" {
+  source             = "../modules/ecs_service"
+  combined_objects   = module.terraform_data.combined_objects
+  env                = var.env
+  subnet_ids         = module.networking.public_subnet_ids
+  security_group_ids = [module.networking.ecs_security_group_id]
+}
+
+# RDS Database Module
+module "rds_database" {
+  source            = "../modules/rds_database"
+  combined_objects  = module.terraform_data.combined_objects
+  env               = var.env
+  db_username       = var.db_username
+  db_password_secret_arn = aws_secretsmanager_secret.db_password.id  # Reference the secret ARN
+  subnet_ids        = module.networking.public_subnet_ids
+  #security_group_id = module.networking.rds_security_group_id
+}
+
+
+module "ec2_instance" {
+  source            = "../modules/ec2_instance"
+  combined_objects  = module.terraform_data.combined_objects
+  env               = var.env
+  subnet_id         = module.networking.public_subnet_ids[0]
+  security_group_id = module.networking.ec2_security_group_id
+}
+
+module "dynamodb_table" {
+  source           = "../modules/dynamodb_table"
+  combined_objects = module.terraform_data.combined_objects
+  env              = var.env
+}
+
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "/${var.env}/db_password"
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = var.db_password  
+}
